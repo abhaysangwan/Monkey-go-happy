d1774b98aac3f51d5cf75397409f500b849a7384
abhaysangwan <71165686+abhaysangwan@users.noreply.github.com>
2021-03-22 18:13:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ var score=0;
 
 function preload(){
   backImage=loadImage("jungle.jpg");
-  player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
+// player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
   
   
 
@@ -30,8 +30,8 @@ function setup() {
   backgr.velocityX=-4;
   
   player = createSprite(100,340,20,50);
-  player.addAnimation("Running",player_running);
-  player.scale = 0.1;
+//   player.addAnimation("Running",player_running);
+//   player.scale = 0.1;
   
   ground = createSprite(400,350,800,10);
   ground.velocityX=-4;
